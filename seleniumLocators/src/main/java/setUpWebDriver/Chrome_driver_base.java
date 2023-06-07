30ba8f802938b70c44387faa99e08d182ccc6ab8
Shakil <shuvroahmed959@gmail.com>
2023-06-07 22:10:11 +0600

    java version update

--- seleniumLocators/src/main/java/setUpWebDriver/Chrome_driver_base.java
+++ seleniumLocators/src/main/java/setUpWebDriver/Chrome_driver_base.java
@@ -1,5 +1,31 @@
 package setUpWebDriver;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Chrome_driver_base {
 
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public static void setDriver() 
+	{
+		//ChromeOptions options = new ChromeOptions();
+		//options.addArguments("--remote-allow-origins=*");
+		
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+	}
+	
+	@AfterSuite
+	public static void closeWindows() 
+	{
+		driver.close();
+		//driver.quit();
+	}
 }
